243103b331a8927371a966a8db5c0f8c9b6e9433
resyin <resyin9b@gmail.com>
2026-07-18 20:45:14 +0700

    feat:add cv download

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ThemeButton } from "../components/ThemeButton";
 import { FaBars, FaTimes } from "react-icons/fa";
+import cv from "../assets/cv-resyin.pdf";
 import "../App.css";
 
 export const Navbar = () => {
@@ -22,11 +23,12 @@ export const Navbar = () => {
     <>
       <nav className="w-full sticky top-0 z-50 flex grid grid-cols-2 items-center justify-center h-15 shadow shadow-black shadow-md/20 dark:bg-[#2D2D2D] md:grid-cols-2 bg-white transition-colors duration-500">
         <div className="flex justify-start pl-5 md:pr-5">
-          <h1 className="font-alfa bg-gradient-to-r from-[#BBDC12] via-[#8ECA3C] to-[#276F27] bg-clip-text text-transparent md:px-10 lg:px-15">resyin.dev</h1>
+          <h1 className="font-alfa bg-gradient-to-r from-[#BBDC12] via-[#8ECA3C] to-[#276F27] bg-clip-text text-transparent md:px-10 lg:px-15">
+            resyin.dev
+          </h1>
         </div>
 
         <div className="hidden md:flex md:justify-end md:px-10 lg:px-15 gap-5 ">
-          
           <ul className="hidden md:flex md:items-center md:font-alice md:gap-5 ">
             <li className="hover:bg-gradient-to-r hover:from-[#BBDC12] hover:to-[#8ECA3C] hover:bg-clip-text hover:text-transparent dark:text-white dark:hover:bg-gradient-to-r dark:hover:from-[#BBDC12] dark:hover:to-[#8ECA3C] dark:hover:bg-clip-text dark:hover:text-transparent">
               <a href="#home">Home</a>
@@ -42,6 +44,11 @@ export const Navbar = () => {
             </li>
             <li className="hover:bg-gradient-to-r hover:from-[#BBDC12] hover:to-[#8ECA3C] hover:bg-clip-text hover:text-transparent dark:text-white dark:hover:bg-gradient-to-r dark:hover:from-[#BBDC12] dark:hover:to-[#8ECA3C] dark:hover:bg-clip-text dark:hover:text-transparent">
               <a href="#contact">Contact</a>
+            </li>
+            <li className="hover:bg-gradient-to-r hover:from-[#BBDC12] hover:to-[#8ECA3C] hover:bg-clip-text hover:text-transparent dark:text-white dark:hover:bg-gradient-to-r dark:hover:from-[#BBDC12] dark:hover:to-[#8ECA3C] dark:hover:bg-clip-text dark:hover:text-transparent">
+              <a href={cv} download="cv-resyin.pdf">
+                Download CV
+              </a>
             </li>
           </ul>
 
